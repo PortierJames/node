60bef3f59ffc91ad2d7feeb9bee0716540003756
PortierJames <james.maiangowi@utoronto.ca>
2016-03-01 22:06:45 -0500

    And another!

---
+++ book.js
@@ -0,0 +1,4 @@
+exports.name = 'On the Origin of Selfish Memes';
+exports.read = function() {
+    console.log('I am reading ' + exports.name);
+}
--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello world");
+	response.write("Hello world!");
 	response.end();
     }).listen(8888);
